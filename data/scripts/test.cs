b0e9e89d6af75ed4d9249769006b90cf8b77e2c6
Fredrik Lindahl <fredrik-lindahl@outlook.com>
2019-03-20 23:17:01 +0100

    wip

--- data/scripts/test.cs
+++ data/scripts/test.cs
@@ -1,9 +1,14 @@
 using System;
- 
+using System.Runtime.InteropServices;
+
 public class HelloWorld
 {
+    [DllImport ("__Internal", EntryPoint="Foobar")]
+    static extern void Foobar ();
+
     static public void Main ()
     {
         Console.WriteLine ("Hello Mono World");
+        Foobar();
     }
 }
